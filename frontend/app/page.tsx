e19ecd15d2214a5097922b1b90b3024aca8ebf75
Derek <109226053+derekjtong@users.noreply.github.com>
2024-08-05 03:11:05 -0400

    nextjs deployment

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,26 @@
+"use client";
 import { TODO_API_URL } from "@/lib/config";
 import axios from "axios";
+import { useEffect, useState } from "react";
 import TodoList from "./components/TodoList";
 
-export default async function Home() {
+async function fetchTodoItems() {
   const res = await axios.get(`${TODO_API_URL}/api/items`);
-  const initialItems = res.data;
+  console.log(`calling ${TODO_API_URL}/api/items`);
+  return res.data;
+}
+
+export default function Home() {
+  const [initialItems, setInitialItems] = useState([]);
+
+  useEffect(() => {
+    async function loadData() {
+      const items = await fetchTodoItems();
+      setInitialItems(items);
+    }
+    loadData();
+  }, []);
+
   return (
     <main className="min-h-screen bg-gray-100 p-4">
       <TodoList initialItems={initialItems} />
